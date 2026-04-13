7224cb2a78194f7f5dcd5166e93e7339de3517cf
kotakotarous <kotakotarous@users.noreply.github.com>
2026-04-13 11:32:33 +0900

    feat: スマホ対応レスポンシブレイアウト（下部ナビ＋全画面詳細）

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3564782449" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2876726110" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
